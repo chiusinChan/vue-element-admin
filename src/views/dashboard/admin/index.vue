a9976037268b90d81deb9016cc72cc08474bfa18
朝鲜咯 <suns2t@163.com>
2018-03-31 18:40:36 +0800

    temsave

--- src/views/dashboard/admin/index.vue
+++ src/views/dashboard/admin/index.vue
@@ -1,43 +1,46 @@
 <template>
-  <div class="dashboard-editor-container">
-    <github-corner></github-corner>
+  <!--<div class="dashboard-editor-container">-->
+    <!--<github-corner></github-corner>-->
 
-    <panel-group @handleSetLineChartData="handleSetLineChartData"></panel-group>
+    <!--<panel-group @handleSetLineChartData="handleSetLineChartData"></panel-group>-->
 
-    <el-row style="background:#fff;padding:16px 16px 0;margin-bottom:32px;">
-      <line-chart :chart-data="lineChartData"></line-chart>
-    </el-row>
+    <!--<el-row style="background:#fff;padding:16px 16px 0;margin-bottom:32px;">-->
+      <!--<line-chart :chart-data="lineChartData"></line-chart>-->
+    <!--</el-row>-->
 
-    <el-row :gutter="32">
-      <el-col :xs="24" :sm="24" :lg="8">
-        <div class="chart-wrapper">
-          <raddar-chart></raddar-chart>
-        </div>
-      </el-col>
-      <el-col :xs="24" :sm="24" :lg="8">
-        <div class="chart-wrapper">
-          <pie-chart></pie-chart>
-        </div>
-      </el-col>
-      <el-col :xs="24" :sm="24" :lg="8">
-        <div class="chart-wrapper">
-          <bar-chart></bar-chart>
-        </div>
-      </el-col>
-    </el-row>
+    <!--<el-row :gutter="32">-->
+      <!--<el-col :xs="24" :sm="24" :lg="8">-->
+        <!--<div class="chart-wrapper">-->
+          <!--<raddar-chart></raddar-chart>-->
+        <!--</div>-->
+      <!--</el-col>-->
+      <!--<el-col :xs="24" :sm="24" :lg="8">-->
+        <!--<div class="chart-wrapper">-->
+          <!--<pie-chart></pie-chart>-->
+        <!--</div>-->
+      <!--</el-col>-->
+      <!--<el-col :xs="24" :sm="24" :lg="8">-->
+        <!--<div class="chart-wrapper">-->
+          <!--<bar-chart></bar-chart>-->
+        <!--</div>-->
+      <!--</el-col>-->
+    <!--</el-row>-->
 
-    <el-row :gutter="8">
-      <el-col :xs="{span: 24}" :sm="{span: 24}" :md="{span: 24}" :lg="{span: 12}" :xl="{span: 12}" style="padding-right:8px;margin-bottom:30px;">
-        <transaction-table></transaction-table>
-      </el-col>
-      <el-col :xs="{span: 12}" :sm="{span: 12}" :md="{span: 12}" :lg="{span: 6}" :xl="{span: 5}">
-        <todo-list></todo-list>
-      </el-col>
-      <el-col :xs="{span: 12}" :sm="{span: 12}" :md="{span: 12}" :lg="{span: 6}" :xl="{span: 5}">
-        <box-card></box-card>
-      </el-col>
-    </el-row>
+    <!--<el-row :gutter="8">-->
+      <!--<el-col :xs="{span: 24}" :sm="{span: 24}" :md="{span: 24}" :lg="{span: 12}" :xl="{span: 12}" style="padding-right:8px;margin-bottom:30px;">-->
+        <!--<transaction-table></transaction-table>-->
+      <!--</el-col>-->
+      <!--<el-col :xs="{span: 12}" :sm="{span: 12}" :md="{span: 12}" :lg="{span: 6}" :xl="{span: 5}">-->
+        <!--<todo-list></todo-list>-->
+      <!--</el-col>-->
+      <!--<el-col :xs="{span: 12}" :sm="{span: 12}" :md="{span: 12}" :lg="{span: 6}" :xl="{span: 5}">-->
+        <!--<box-card></box-card>-->
+      <!--</el-col>-->
+    <!--</el-row>-->
 
+  <!--</div>-->
+  <div>
+    <h1>    Wellcome</h1>
   </div>
 </template>
 
